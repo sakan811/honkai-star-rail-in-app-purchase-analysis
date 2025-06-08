944c3a8242e91742255a700478ecbda48d33c53d
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-06-08 21:06:13 +0700

    fix: add missing comma in nuxt.config.ts modules array

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-charts']
+  modules: ['@nuxt/ui', 'nuxt-charts'],
+  css: ['~/assets/css/main.css']
 })
